5b4c7d47e3b3ff6e220c2bd6c951e34a40b80709
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2017-12-28 04:20:14 -0700

    tutorial 1 implemented

--- caffe_tutorial/tutorial1.py
+++ caffe_tutorial/tutorial1.py
@@ -5,6 +5,10 @@ referred from:
 '''
 
 import caffe
+import numpy as np
+import cv2
+from PIL import Image
+
 
 # tell caffe we want to use GPU
 caffe.set_device(0)
@@ -13,6 +17,7 @@ caffe.set_mode_gpu()
 # get the net from prototxt file
 net = caffe.Net('tutorial1convnet.prototxt', caffe.TEST)
 
+# printing some...
 print("\nnet.inputs:", net.inputs)
 print("\ndir(net.blobs):", dir(net.blobs))
 print("\ndir(net.params):", dir(net.params))
@@ -21,3 +26,20 @@ print("\nnet.blobs['data'].data:", net.blobs['data'].data)
 print("\nnet.blobs['conv'].data.shape:", net.blobs['conv'].data.shape)
 for i in range(0, len(net.params['conv'])):
 	print("\nnet.params['conv']["+str(i)+"]:", net.params['conv'][i])
+
+img = np.array(Image.open('cat_gray.jpg'))		# open and load the image in img
+img_input = img[np.newaxis, np.newaxis, :, :]	# reshapes the input to shape (1,1,360,480)
+print(img_input)								# prints the pixels value (which have shape (1,1,360,480))
+print(img_input.shape)							# prints (1,1,360,480)
+print(*img_input.shape)							# prints 1,1,360,480
+net.blobs['data'].reshape(*img_input.shape)		# reshape 'data' to current image dimensions 1,1,360,480
+net.blobs['data'].data[...] = img_input			# then assign actual data of 'data' as pixels value
+
+# now make a forward pass since we have assigned the data
+net.forward()
+
+# Now net.blobs['conv'] is filled with data, and the 10 activation maps (net.blobs['conv'].data[0,i]) can be plotted.
+cv2.imwrite('output_image_' + str(0) + '.jpg', 255*net.blobs['conv'].data[0,0]) # using i=0 only, but can iterate from 0 to 9
+
+# To save the net parameters i.e. net.params, just call :
+net.save('tutorial1.caffemodel')
