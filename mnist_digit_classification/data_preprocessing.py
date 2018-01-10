2acc594dc7caa7b2f6b0a694e9203c1158d95c55
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2018-01-09 23:25:16 -0700

    implemented and tested loading dataset and converting to hdf5

--- mnist_digit_classification/data_preprocessing.py
+++ mnist_digit_classification/data_preprocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+import h5py
 
 def get_dataset_features_labels_np(dataset_path):
     dataset = []
@@ -19,7 +20,7 @@ def get_dataset_features_labels_np(dataset_path):
             dataset.append(temp_line)
 
     dataset = np.array(dataset)
-    dataset_features = np.array(dataset[:, 1:], dtype='float')
+    dataset_features = np.array(dataset[:, 1:], dtype=np.float32)
     dataset_labels_temp = np.array(dataset[:, 0], dtype='int')
 
     dataset_labels = []
@@ -27,8 +28,16 @@ def get_dataset_features_labels_np(dataset_path):
         temp = np.zeros(10, dtype='int')    # number of classes is 10
         temp[int(element)] = 1
         dataset_labels.append(temp)
-    dataset_labels = np.array(dataset_labels, dtype='int')
+    dataset_labels = np.array(dataset_labels, dtype=np.float32)
 
     return dataset_features.reshape((dataset_features.shape[0],1,28,28)), dataset_labels
 
 def convert_to_HDF5(dataset_path, hdf5_filename):
+    dataset_features, dataset_labels = get_dataset_features_labels_np(dataset_path)
+    print('dataset_features.shape:', dataset_features.shape,' dataset_labels.shape:', dataset_labels.shape)
+
+    with h5py.File(hdf5_filename, 'w') as f:
+        f['data'] = dataset_features
+        f['label'] = dataset_labels
+
+    print("Saved as HDF5")
--- mnist_digit_classification/main.py
+++ mnist_digit_classification/main.py
@@ -3,6 +3,7 @@ references from:
 	http://caffe.berkeleyvision.org/gathered/examples/mnist.html
 	https://github.com/nitnelave/pycaffe_tutorial
 	https://stackoverflow.com/questions/32379878/cheat-sheet-for-caffe-pycaffe
+	https://github.com/Franck-Dernoncourt/caffe_demos/blob/master/iris/iris_tuto.py
 '''
 
 '''
@@ -19,28 +20,30 @@ from data_preprocessing import convert_to_HDF5
 #--- Step 1) ---
 
 dataset_train_path = 'G:/DL/mnist_data_for_caffe/train.csv'
+dataset_train_hdf5_path = 'G:/DL/mnist_data_for_caffe/dataset_train.hdf5'
+convert_to_HDF5(dataset_train_path, dataset_train_hdf5_path)
 
-USE_GPU = True
-if USE_GPU:
-    caffe.set_device(0)
-    caffe.set_mode_gpu()
-else:
-    caffe.set_mode_cpu()
+# USE_GPU = True
+# if USE_GPU:
+#     caffe.set_device(0)
+#     caffe.set_mode_gpu()
+# else:
+#     caffe.set_mode_cpu()
 
-CNN_NETWORK_PATH = "G:/DL/caffe_practice/mnist_digit_classification/cnn_network.prototxt"
-CNN_SOLVER_PATH = "G:/DL/caffe_practice/mnist_digit_classification/cnn_solver.prototxt"
+# CNN_NETWORK_PATH = "G:/DL/caffe_practice/mnist_digit_classification/cnn_network.prototxt"
+# CNN_SOLVER_PATH = "G:/DL/caffe_practice/mnist_digit_classification/cnn_solver.prototxt"
 
-net = caffe.Net(CNN_NETWORK_PATH, caffe.TRAIN) # caffe.TEST for testing
-solver = caffe.AdamSolver(CNN_SOLVER_PATH)
+# net = caffe.Net(CNN_NETWORK_PATH, caffe.TRAIN) # caffe.TEST for testing
+# solver = caffe.AdamSolver(CNN_SOLVER_PATH)
 
-print("Network layers information:")
-for name, layer in zip(net._layer_names, net.layers):
-    print("{:<7}: {:17s}({} blobs)".format(name, layer.type, len(layer.blobs)))
-print("Network blobs information:")
-for name, blob in net.blobs.items():
-    print("{:<7}: {}".format(name, blob.data.shape))
-print('net.inputs:', net.inputs)
-print('net.outputs:', net.outputs)
+# print("Network layers information:")
+# for name, layer in zip(net._layer_names, net.layers):
+#     print("{:<7}: {:17s}({} blobs)".format(name, layer.type, len(layer.blobs)))
+# print("Network blobs information:")
+# for name, blob in net.blobs.items():
+#     print("{:<7}: {}".format(name, blob.data.shape))
+# print('net.inputs:', net.inputs)
+# print('net.outputs:', net.outputs)
 
-solver.solve()
+# solver.solve()
 
