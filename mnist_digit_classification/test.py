bda744df99846c294b12f8b2100185b9934eccaa
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2018-01-11 00:33:49 -0700

    implementing deployment code, fixed some bugs

--- mnist_digit_classification/test.py
+++ mnist_digit_classification/test.py
@@ -1,5 +1,5 @@
 import caffe
-import os
+import numpy as np
 
 from data_preprocessing import get_test_dataset_features
 
@@ -11,8 +11,9 @@ def get_batch(dataset, i, BATCH_SIZE):
 
 DATASET_TEST_PATH = 'G:/DL/mnist_data_for_caffe/test.csv'
 dataset_test_features = get_test_dataset_features(DATASET_TEST_PATH, normalize=True)
+print('dataset_test_features.shape:', dataset_test_features.shape)
 NUM_EXAMPLES = dataset_test_features.shape[0]
-BATCH_SIZE = 64
+BATCH_SIZE = 100
 
 USE_GPU = True
 if USE_GPU:
@@ -21,9 +22,22 @@ if USE_GPU:
 else:
     caffe.set_mode_cpu()
 
-CNN_NETWORK_PATH = "G:/DL/caffe_practice/mnist_digit_classification/cnn_network.prototxt"
+CNN_NETWORK_PATH = "G:/DL/caffe_practice/mnist_digit_classification/cnn_deploy_network.prototxt"
 CAFFEMODEL_PATH =  "G:/DL/mnist_data_for_caffe/snapshot_iter_2500.caffemodel"
 net = caffe.Net(CNN_NETWORK_PATH, CAFFEMODEL_PATH, caffe.TEST)
 
+i_= 0
+first_flag = True
+predicted_labels = []
 for i in range(0, int(NUM_EXAMPLES/BATCH_SIZE)):
-	pass
+	i_=i
+	batch_x = get_batch(dataset_test_features, i, BATCH_SIZE)
+	predicted_batch_x = net.forward(data=batch_x)
+
+	if first_flag:
+		print(predicted_batch_x)
+		predicted_labels = predicted_batch_x['prob']
+		first_flag = False
+	else:
+		predicted_labels = np.concatenate((predicted_labels, predicted_batch_x['prob']), axis=0)
+
